86da53399bd18cd5a514dcaa7a823130e723ccca
Lee <znlspf123@naver.com>
2022-07-02 23:05:12 +0900

    KNUT-4 [Server] Mount Backend Project // 프로젝트 재설치

---
+++ backend/knutu/run.cmd
@@ -0,0 +1,2 @@
+@echo off
+./mvnw spring-boot:run
--- backend/knutu/src/test/java/knutu/knutu/KnutuApplicationTests.java
+++ backend/knutu/src/test/java/knutu/knutu/KnutuApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package knutu.knutu;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class KnutuApplicationTests {
 
 	@Test
 	void contextLoads() {
