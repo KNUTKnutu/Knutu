37f4228722c119e906696325ab5e48b3eb1ca143
Lee <znlspf123@naver.com>
2022-07-03 16:27:11 +0900

    KNUT-22 [Client] Suspense // 초기 골격

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,20 @@ import "./App.css";
 import IntroScene from "./Scenes/IntroScene/IntroScene";
 import LobbyScene from "./Scenes/LobbyScene/LobbyScene";
 import GameScene from "./Scenes/GameScene/GameScene";
+import { Suspense } from "react";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header></header>
-      <main>
-        <IntroScene />
-        <LobbyScene />
-        <GameScene />
-      </main>
-      <footer></footer>
+      <Suspense fallback={suspenseComponent} >
+        <header></header>
+        <main>
+          <IntroScene />
+          <LobbyScene />
+          <GameScene />
+        </main>
+        <footer></footer>
+      </Suspense>
     </div>
   );
 };
