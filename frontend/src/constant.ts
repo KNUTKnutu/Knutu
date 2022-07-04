b2523c58cbef383a48ab83c57c1e920eef5cb1a5
Lee <znlspf123@naver.com>
2022-07-04 14:25:23 +0900

    constants

--- frontend/src/constant.ts
+++ frontend/src/constant.ts
@@ -1,1 +1,5 @@
 export const FAKEAPI = "https://jsonplaceholder.typicode.com/todos/1";
+
+export const SCENE__INTROSCENE = "IntroScene";
+export const SCENE__LOBBYSCENE = "LobbyScene";
+export const SCENE__GAMESCENE = "GameScene";
