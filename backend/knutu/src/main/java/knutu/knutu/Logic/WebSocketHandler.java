14141b96dc45e6c749716a84e49a600cee53b350
Lee <znlspf123@naver.com>
2022-07-22 19:13:45 +0900

    KNUT-36 [Server] Connect WebSocket to Client

---
+++ backend/knutu/src/main/java/knutu/knutu/Logic/WebSocketHandler.java
@@ -0,0 +1,45 @@
+package knutu.knutu.Logic;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@ServerEndpoint(value="/ws")
+public class WebSocketHandler {
+
+    private static Set<Session> clients = Collections.synchronizedSet(new HashSet<Session>());
+
+    @OnMessage
+    public void onMessage(String msg, Session session) throws Exception {
+        log.info("onMessage");
+        log.info(msg);
+        log.info(session.toString());
+        log.info(clients.toString());
+    }
+	
+	@OnOpen
+	public void onOpen(Session session) {
+        log.info("onOpen");
+        log.info(session.toString());
+        log.info(clients.toString());
+	}
+	
+	@OnClose
+	public void onClose(Session session) {
+        log.info("onClose");
+        log.info(session.toString());
+        log.info(clients.toString());
+	}
+    
+}
