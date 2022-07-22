580d3d4b357ef65080ebf120c51428826cd088cc
Lee <znlspf123@naver.com>
2022-07-22 19:30:00 +0900

    KNUT-12 [Client] Font Files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,6 @@ import { Suspense } from "react";
 import Main from "./Components/Main/Main";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
-import { io } from "socket.io-client";
 
 // const socket = io(`http://localhost:8080`);
 // socket.emit("Test", { Classes: "Test", Text: "Testing now" });
