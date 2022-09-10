1c98b20d8867f925f170becffa6601977ebb5a4d
Lee <znlspf123@naver.com>
2022-09-10 13:38:19 +0900

    KNUT-31 [Server] Fallback Returning With Some Specific Status Codes

--- frontend/src/constant.ts
+++ frontend/src/constant.ts
@@ -20,3 +20,11 @@ export const LOGOUT = "로그아웃";
 export const LANGUAGE__KOREAN: string = "Korean";
 export const LANGUAGE__ENGLISH: string = "English";
 export const LANGUAGE__DEFAULT: string = LANGUAGE__KOREAN;
+
+// status codes
+export const STATUSCODE__OK: number = 200;                  // 정상 동작
+export const STATUSCODE__BAD_REQUEST: number = 400;         // Request에 잘못된 필드나 이상한 필드가 섞여서 넘어가서 서버에서 수상하다고 Response를 에러로 주는 상황
+export const STATUSCODE__UNAUTHORIZED: number = 401;        // 입력된 정보가 잘못됨
+export const STATUSCODE__PAGE_NOT_FOUND: number = 404;      // 페이지를 찾을 수 없음
+export const STATUSCODE__INTERNAL_SERVER_ERROR: number = 500;   // 서버가 다운된 상태
+export const STATUSCODE__BAD_GATEWAY: number = 502;         // 서버의 프록시 설정이나 리다이렉션, nginx 등이 뭔가 문제가 있어서 Response를 줄라니 배배 꼬여 이상해져서 Response를 아싸리 안 주려고 결정한 상황
