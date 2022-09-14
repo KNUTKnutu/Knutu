dfaeb8c3a72983ea3f78a15e2b066b96ebe78be8
민경호 <1736s@naver.com>
2022-09-14 13:43:43 +0900

    프로필 오류 시 대체 이미지로 변경

--- frontend/src/Components/Main/Scenes/IntroScene/LoginSide/Profile/Profile.tsx
+++ frontend/src/Components/Main/Scenes/IntroScene/LoginSide/Profile/Profile.tsx
@@ -1,44 +1,29 @@
 import { useSetRecoilState } from "recoil";
 import { LOGOUT, TITLE } from "../../../../../../constant";
 import { LOGINSTATE } from "../../../../../../enum";
+import { Nullable, User } from "../../../../../../interface";
 import { userState } from "../../../../../../Recoil/atom";
 import styles from "../../../../../../Styles/Components/Main/Scenes/IntroScene/LoginSide/Profile/_profile.module.scss";
+import DEFAULT_PROFILE from "../../../../../../Assets/Images/default_profile.svg";
 
 interface Props {
   setCurrLoginState: React.Dispatch<React.SetStateAction<LOGINSTATE>>;
-  name: string;
-  title: string;
-  profilePicture: string;
-  level: number;
-  currentExperience: number;
-  isAccountgaemaeneo: boolean;
+  user: Nullable<User>;
 }
 
-const ThumbUp = (
-  <span className="material-icons" style={{ color: "blue" }}>
-    thumb_up_alt
-  </span>
-);
-const ThumbDown = (
-  <span className="material-icons" style={{ color: "red" }}>
-    thumb_down_alt
-  </span>
-);
-
-const Profile = ({
-  setCurrLoginState,
-  name,
-  title,
-  profilePicture,
-  level,
-  currentExperience,
-  isAccountgaemaeneo,
-}: Props) => {
+const Profile = ({ setCurrLoginState, user }: Props) => {
+  const { name, title, profilePicture, level, currentExperience } =
+    user as User;
   const setUser = useSetRecoilState(userState);
 
   const onClickLogout = () => {
     setCurrLoginState(LOGINSTATE.LOGIN);
     setUser(null);
+  };
+
+  // img가 엑박 뜰 경우 대체 이미지 설정
+  const onErrorImg = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    e.currentTarget.src = DEFAULT_PROFILE;
   };
   return (
     <div className={styles.profile}>
@@ -47,7 +32,7 @@ const Profile = ({
       </div>
       <div className={styles.player}>
         <div className={styles.sub}>
-          <img src={profilePicture} alt="profilePicture" />
+          <img src={profilePicture} alt="profilePicture" onError={onErrorImg} />
           <span className={styles.title}>{title}</span>
         </div>
         <div className={styles.main}>
