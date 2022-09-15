f2f5d94c91ac027f5a0bc624ddb13a94ee759fa9
KangMin Her <81793748+nicecarrot@users.noreply.github.com>
2022-09-15 13:50:27 +0900

    Regex NickName Revise

--- frontend/src/Logic/Regex/regex.ts
+++ frontend/src/Logic/Regex/regex.ts
@@ -12,7 +12,7 @@ export const RegexPw: RegExp = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%&*?])[A-Za-z\d
 // 닉네임
 // 1. 2자 이상 10자 이하
 // 2. 한글 가능, 영어 대소문자 가능, 숫자 가능, 특수문자 불가능
-export const RegexName: RegExp = /^[a-zA-Z가-힣0-9]{2,10}$/gi;
+export const RegexName: RegExp = /^(?=.*[a-zA-Z\dㄱ-ㅎㅏ-ㅣ가-힣])[A-Za-z\dㄱ-ㅎㅏ-ㅣ가-힣]{2,10}$/gi;
 
 // 이메일
 // 
