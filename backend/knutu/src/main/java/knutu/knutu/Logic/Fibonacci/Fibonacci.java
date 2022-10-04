9d6962176873d5df0f2cc09757ea537d6de810db
yeojin011028 <53716750+yeojin011028@users.noreply.github.com>
2022-10-04 23:23:25 +0900

    KNUT - 111 [Server] Hash Experience Table

--- backend/knutu/src/main/java/knutu/knutu/Logic/Fibonacci/Fibonacci.java
+++ backend/knutu/src/main/java/knutu/knutu/Logic/Fibonacci/Fibonacci.java
@@ -1,6 +1,9 @@
 package knutu.knutu.Logic.Fibonacci;
 
+import java.util.HashMap;
+
 public class Fibonacci {
+    public static HashMap<Integer, Double> experienceTable = new HashMap<Integer, Double>();
     public static double getFibonacci(int index) {
         if(index == 1 || index == 2) return 1;
 
@@ -20,10 +23,15 @@ public class Fibonacci {
     // 황여진 TODO: Experience Table을 만들고, 거기에 Hash 필요
     // 황여진 TODO: 직접 Fibonacci를 Brute Force로 10 내지는 20정도까지 구해보고 아래 함수가 참인지 판별 요청.
     public static double getTotalExperience(int level) {
+    	if(experienceTable.containsKey(level)) return experienceTable.get(level);
+
         if(level == 0) return 0;
         if(level == 1) return 1;
         
-        return Fibonacci.getFibonacci(level) + Fibonacci.getFibonacci(level - 1);
+        double result = Fibonacci.getFibonacci(level) + Fibonacci.getFibonacci(level - 1);
+        experienceTable.put(level, result);
+
+        return result;
     }
 
     // 황여진 TODO: 위의 함수들을 이용하여, 현재 User의 currExperience를 매개변수로 받아 User의 Level을 구하여 int type으로 return하는 아래 메소드 구현.
