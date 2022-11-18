378d2f3f9c6bf3c132c0228ba5cd1bd11275e5d2
yeojin011028 <53716750+yeojin011028@users.noreply.github.com>
2022-11-18 11:59:15 +0900

    KNUT-31 [Server] Fallback Returning With Some Specific Status Codes

--- frontend/src/constant.ts
+++ frontend/src/constant.ts
@@ -31,9 +31,14 @@ export const LANGUAGE__DEFAULT: LanguageNameType = LANGUAGE__KOREAN;
 export const STATUSCODE__OK: ResponseStatusCode = 200; // 정상 동작
 export const STATUSCODE__BAD_REQUEST: ResponseStatusCode = 400; // Request에 잘못된 필드나 이상한 필드가 섞여서 넘어가서 서버에서 수상하다고 Response를 에러로 주는 상황
 export const STATUSCODE__UNAUTHORIZED: ResponseStatusCode = 401; // 입력된 정보가 잘못됨
+export const STATUSCODE__FORBIDDEN: ResponseStatusCode = 403; // 리소스에 대한 권한 부족
 export const STATUSCODE__PAGE_NOT_FOUND: ResponseStatusCode = 404; // 페이지를 찾을 수 없음
+export const STATUSCODE__METHOD_NOT_ALLOWED: ResponseStatusCode = 405; // 허가 되지 않은 방식의 요청
+export const STATUSCODE__REQUEST_TIMEOUT: ResponseStatusCode = 408; // 요청 대기시간이 지난 상태
 export const STATUSCODE__INTERNAL_SERVER_ERROR: ResponseStatusCode = 500; // 서버가 다운된 상태
 export const STATUSCODE__BAD_GATEWAY: ResponseStatusCode = 502; // 서버의 프록시 설정이나 리다이렉션, nginx 등이 뭔가 문제가 있어서 Response를 줄라니 배배 꼬여 이상해져서 Response를 아싸리 안 주려고 결정한 상황
+export const STATUSCODE__SERVICE_UNAVAILABLE: ResponseStatusCode = 503; // 서비스가 이용 불가능함
+export const STATUSCODE__LOOP_DETECTED: ResponseStatusCode = 508; // 서버가 요청을 처리할 때 무한 루프 감지
 
 // MAKE ROOM
 export const MAXIMUM = [2, 3, 4, 5, 6, 7, 8];
