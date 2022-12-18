3a994812a086e31131f40d3239f66d669612e4be
Lee <znlspf123@naver.com>
2022-12-18 14:52:13 +0900

    KNUT-126 [Client] KnutuCommonLib - wakeUp() => onHold

--- frontend/src/Logic/Library/KnutuCommonLib/KnutuCommonLib.ts
+++ frontend/src/Logic/Library/KnutuCommonLib/KnutuCommonLib.ts
@@ -2,7 +2,9 @@ class KnutuCommonLib {
   public static commomLib: KnutuCommonLib = new KnutuCommonLib();
   public static getInstance = (): KnutuCommonLib => this.commomLib;
 
-  public wakeUp = () => {};
+  public wakeUp = () => {
+    /* 보류 */
+  };
 }
 
 export default KnutuCommonLib;
