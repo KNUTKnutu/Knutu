73e6e8fc76845342ff0ed6b1cca2ab4ec1edcf66
Lee <znlspf123@naver.com>
2022-12-30 13:13:39 +0900

    GameScene Waiting - 유저 창의 정보가 제대로 바인딩 되도록

--- frontend/src/Components/Main/Scenes/GameScene/GameScene__Waiting/components/UserInfo.tsx
+++ frontend/src/Components/Main/Scenes/GameScene/GameScene__Waiting/components/UserInfo.tsx
@@ -1,23 +1,32 @@
 import styles from "../../../../../../styles/Components/Main/Scenes/GameScene/Wating/_gameWating.module.scss";
+import knutu from "/src/Assets/Images/Knutu_64x64.jpg";
 import { userState } from "../../../../../../Recoil/atom";
-import { useRecoilState } from "recoil";
+import { useRecoilValue } from "recoil";
+
+const getLevelString = (level: number): string => `Level ${level}`;
+const getExperienceString = (currentExp: number, remainingExp: number): string => `${currentExp} / ${remainingExp}`;
 
 const UserInfo = () => {
-  // const [user, setUser] = useRecoilState<Nullable<User>>(userState);
-  const user = { name: "샌슨 퍼시발", level: 6 };
-  const { name, level } = user!;
+
+  const user = useRecoilValue(userState);
+
+  const { name, level, profilePicture, currentExperience, remainExperience }: any = user;
+
+  if(profilePicture) {
+    // 프로필 사진을 knutu가 아닌 다른 거로 가져오는 로직
+  }
 
   return (
     <div className={styles.userinfo}>
       <img
-        src="/src/Assets/Images/Knutu_64x64.jpg"
+        src={knutu}
         className={styles.user_profile}
       />
-      <span className={styles.user_level}>Level {level}</span>
+      <span className={styles.user_level}>{getLevelString(level)}</span>
       <span className={styles.user_name}>{name}</span>
       <div className={styles.expbar}>
         <div className={styles.game_expFillColor} />
-        <div className={styles.game_exp}>41,920 / 104,800</div>
+        <div className={styles.game_exp}>{getExperienceString(currentExperience, remainExperience)}</div>
       </div>
     </div>
   );
