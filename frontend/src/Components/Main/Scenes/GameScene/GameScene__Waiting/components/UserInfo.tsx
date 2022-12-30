10ada47ec5c9974d1bcca7f7f7ddf25ce5c35abe
Lee <znlspf123@naver.com>
2022-12-30 13:27:49 +0900

    LobbyScene, GameScene -> 경험치 바 로직 적용

--- frontend/src/Components/Main/Scenes/GameScene/GameScene__Waiting/components/UserInfo.tsx
+++ frontend/src/Components/Main/Scenes/GameScene/GameScene__Waiting/components/UserInfo.tsx
@@ -4,13 +4,14 @@ import { userState } from "../../../../../../Recoil/atom";
 import { useRecoilValue } from "recoil";
 
 const getLevelString = (level: number): string => `Level ${level}`;
-const getExperienceString = (currentExp: number, remainingExp: number): string => `${currentExp} / ${remainingExp}`;
+const getExperienceString = (currentExp: number, totalExperience: number): string => `${currentExp} / ${totalExperience}`;
+const getExpBarWidth = (currExp: number, totalExp: number): string => `${currExp / totalExp * 100}%`;
 
 const UserInfo = () => {
 
   const user = useRecoilValue(userState);
 
-  const { name, level, profilePicture, currentExperience, remainExperience }: any = user;
+  const { name, level, profilePicture, currentExperience, totalExperience }: any = user;
 
   if(profilePicture) {
     // 프로필 사진을 knutu가 아닌 다른 거로 가져오는 로직
@@ -25,8 +26,8 @@ const UserInfo = () => {
       <span className={styles.user_level}>{getLevelString(level)}</span>
       <span className={styles.user_name}>{name}</span>
       <div className={styles.expbar}>
-        <div className={styles.game_expFillColor} />
-        <div className={styles.game_exp}>{getExperienceString(currentExperience, remainExperience)}</div>
+        <div className={styles.game_expFillColor} style={{width: getExpBarWidth(currentExperience, totalExperience)}} />
+        <div className={styles.game_exp}>{getExperienceString(currentExperience, totalExperience)}</div>
       </div>
     </div>
   );
--- frontend/src/Components/Main/Scenes/LobbyScene/Components/MyInfos.tsx
+++ frontend/src/Components/Main/Scenes/LobbyScene/Components/MyInfos.tsx
@@ -1,7 +1,15 @@
 import styles from "../../../../../Styles/Components/Main/Scenes/LobbyScene/_lobbyScene.module.scss";
+import knutu from "/src/Assets/Images/Knutu_64x64.jpg";
 import { userState } from "../../../../../Recoil/atom";
 import { User } from "../../../../../interface";
 import { useRecoilValue } from "recoil";
+
+const AVATAR_CONF__WIDTH = "64px";
+const AVATAR_CONF__HEIGHT = "64px";
+
+const getDisplayName = (level: number, title: string, name: string): string => `레벨 ${level} ${title} ${name}`;
+const getDisplayExperience = (currExp: number, totalExp: number): string => `${currExp} / ${totalExp}`;
+const getExpBarWidth = (currExp: number, totalExp: number): string => `${currExp / totalExp * 100}%`;
 
 const MyInfos = (): JSX.Element => {
   // const [user, setUser] = useRecoilState<Nullable<User>>(userState);
@@ -17,22 +25,22 @@ const MyInfos = (): JSX.Element => {
       <div className={styles.lobby_scene_myInfos__top}>
         <div className={styles.lobby_scene_myInfos__character}>
           <img
-            src="/src/Assets/Images/Knutu_64x64.jpg"
-            width="64px"
-            height="64px"
+            src={knutu}
+            width={AVATAR_CONF__WIDTH}
+            height={AVATAR_CONF__HEIGHT}
           />
         </div>
         <div className={styles.lobby_scene_myInfos__infoContainer}>
           <div className={styles.lobby_scene_myInfos__info}>
             <span>
-              레벨 {level} {title} {name}
+              {getDisplayName(level, title, name)}
             </span>
           </div>
         </div>
       </div>
       <div className={styles.lobby_scene_myInfos__bottom}>
-        <div className={styles.lobby_scene_myInfos__expFillColor} />
-        <div className={styles.lobby_scene_myInfos__exp}>{currentExperience} / {totalExperience}</div>
+        <div className={styles.lobby_scene_myInfos__expFillColor} style={{width: getExpBarWidth(currentExperience, totalExperience)}} />
+        <div className={styles.lobby_scene_myInfos__exp}>{getDisplayExperience(currentExperience, totalExperience)}</div>
       </div>
     </div>
   );
