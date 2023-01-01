c01f88872e1879ae4d5760350ce90722ea7b488a
Lee <znlspf123@naver.com>
2023-01-01 14:08:39 +0900

    Update Conflict.java

--- backend/knutu/src/main/java/knutu/knutu/Controller/Exceptions/Conflict.java
+++ backend/knutu/src/main/java/knutu/knutu/Controller/Exceptions/Conflict.java
@@ -3,8 +3,8 @@ package knutu.knutu.Controller.Exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-// 401
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
+// 409
+@ResponseStatus(HttpStatus.CONFLICT)
 public class Conflict extends RuntimeException {
     public Conflict(String message) {
         super(message);
