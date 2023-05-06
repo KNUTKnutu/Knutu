3185197d9e7df59af18c8e46fe61cc6ebbcc64cc
Moon SoHee <qwerpoiu91@naver.com>
2023-05-07 04:00:58 +0900

    [Internship Course][Client] Implement "빠른 입장" button on LobbyScene

--- frontend/src/Components/Main/Scenes/LobbyScene/QuickSearchRoom.tsx
+++ frontend/src/Components/Main/Scenes/LobbyScene/QuickSearchRoom.tsx
@@ -1,43 +1,109 @@
-import { Dispatch, FormEvent, SetStateAction } from "react";
+import { Dispatch, FormEvent, SetStateAction, useState } from "react";
 import styles from "../../../../Styles/Components/Main/Scenes/LobbyScene/_quickSearch.module.scss";
-  
+import { currentSceneState, enteredRoomIdState, enteredRoomState, roomsState, userState } from "../../../../Recoil/atom";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { RoomClass } from "./Components/Rooms/Class/Room";
+import { checkRoomEnterable } from "../../../../Logic/API/GET/get";
+import { SCENE__GAMESCENE } from "../../../../constant";
+import { postEnterRoom } from "../../../../Logic/API/POST/post";
+import { Room } from "../../../../interface";
+
 interface Props {
-    setIsShow: Dispatch<SetStateAction<boolean>>;
+  setIsShow: Dispatch<SetStateAction<boolean>>;
 }
 
 const QuickSearchRoom = ({ setIsShow }: Props) => {
+  const roomState = useRecoilValue(enteredRoomState);
+  const { number: roomNumber } = roomState;
+  const user = useRecoilValue(userState);
 
-    const onSubmit = (event: FormEvent<HTMLFormElement>): void => {
-        event.preventDefault();
+  const [roomNumberString, setRoomNumberString] = useState<string>(
+    RoomClass.getRoomNumberString(roomNumber)
+  );
+  const [roomTitleString, setRoomTitleString] = useState("");
+  const [quickRoom, setQuickRoom] = useState<Room | null>(null);
 
-        /* entering room logic below */
-        
-        /* entering room logic above */
-    }
+  const setEnteredRoomIdState = useSetRecoilState(enteredRoomIdState);
+  const setCurrentScene = useSetRecoilState(currentSceneState);
 
-    return (
-        <div className={styles.quickSearchRoom}>
-            <div className={styles.title}>
-                <h3>빠른 입장</h3>
+  let rooms = useRecoilValue(roomsState);
+  let roomValues = Object.entries(rooms);
+    
+  if (Array.isArray(roomValues) && quickRoom === null) {
+    let quickEnterableRooms: Room[] = [];
+
+    roomValues
+      .filter((el) => el[1].currEntry / el[1].maxEntry < 1 && !el[1].pw)
+      .forEach((el) => quickEnterableRooms.push(el[1]));
+
+    quickEnterableRooms.sort((a: any, b: any) => {
+      if (a.number > b.number) return 1;
+      if (a.number < b.number) return -1;
+      return 0;
+    });
+    
+    const { number, title } = quickEnterableRooms[0];
+    setQuickRoom(() => quickEnterableRooms[0]);
+    setRoomNumberString(RoomClass.getRoomNumberString(number));
+    setRoomTitleString(title);
+  }
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+
+    if (quickRoom === null) return;
+
+    const roomToEnter: Room = quickRoom;
+    const { number } = roomToEnter;
+    checkRoomEnterable(number).then((res) => {
+      if (res?.status !== 200) window.alert("방에 입장할 수 없습니다. 잠시 후 다시 시도해주세요.");
+      postEnterRoom(number, user).then((res) => {
+        setEnteredRoomIdState(number);
+        setCurrentScene(SCENE__GAMESCENE);
+        setIsShow(false);
+      });
+    });
+  };
+
+  return (
+    <div className={styles.quickSearchRoom}>
+      <div className={styles.title}>
+        <h3>빠른 입장</h3>
+      </div>
+      <div className={styles.mid_box}></div>
+      <div className={styles.main}>
+        <form onSubmit={onSubmit}>
+          {quickRoom !== null ? (
+            <div>
+              <div className = {styles.message_font_size}>
+                {roomNumberString}번 방으로 입장합니다.
+              </div>
+              <div className = {styles.title_font_size}>
+                방 제목: {roomTitleString}
+              </div>
+              <div className={styles.btn_wrapper}>
+                <button type="submit">입장</button>
+                <button
+                  type="button"
+                  onClick={(prev) => setIsShow(!prev)}>
+                  나가기
+                </button>
+              </div>
             </div>
-            <div className={styles.main}>
-                <form onSubmit={onSubmit}>
-                    <div style={{fontSize: "1.25rem"}}>
-                        001번 방으로 입장합니다.
-                    </div>
-                    <div style={{fontSize: "1rem"}}>
-                        방 제목: TestingTesting1
-                    </div>
-                    <div className={styles.btn_wrapper}>
-                        <button type="submit">입장</button>
-                        <button type="button" onClick={(prev) => setIsShow(!prev)}>
-                            나가기
-                        </button>
-                    </div>
-                </form>
+          ) : (
+            <div className = {styles.message_font_size}>
+              입장 가능한 방이 없습니다.
+              <div className={styles.btn_wrapper}>
+                <button type="button" onClick={(prev) => setIsShow(!prev)}>
+                  나가기
+                </button>
+              </div>
             </div>
-        </div>
-    );
+          )}
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default QuickSearchRoom;
