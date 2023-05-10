ee3d2e837f343709d09f56a64956844abf8327b6
Lee <znlspf123@naver.com>
2023-05-10 18:27:55 +0900

    KNUT-153 [Server] Allow All Possiblities Relavant with CORS Policy

---
+++ backend/knutu_backend/API/Middleware/CORS.go
@@ -0,0 +1,19 @@
+package middleware
+
+import "github.com/gin-gonic/gin"
+
+func SetCORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization, Origin")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
