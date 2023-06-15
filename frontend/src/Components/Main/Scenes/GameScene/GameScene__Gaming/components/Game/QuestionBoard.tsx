309fee1840de18653d66895b18872b713ab85d83
Lee <znlspf123@naver.com>
2023-06-15 23:06:51 +0900

    hand over to Carrot

--- frontend/src/Components/Main/Scenes/GameScene/GameScene__Gaming/components/Game/QuestionBoard.tsx
+++ frontend/src/Components/Main/Scenes/GameScene/GameScene__Gaming/components/Game/QuestionBoard.tsx
@@ -57,18 +57,18 @@ const QuestionBoard = () => {
 
   useEffect(() => {
     const timerInterval: NodeJS.Timer = setInterval(() => {
-      if(remainTurnTimeNumber === 0) {
-        if (room.currTurn === user?.name && RoundInProgress) {
+      if(remainTurnTimeNumber === 0 && RoundInProgress) {
+        if(room.currTurn === user?.name) {
           const payload = KnutuWebSocketHandler.getInstance().wrapPacket("onRoundEnd", {
             roomId: room.number,
             userName: user?.name
           });
           KnutuWebSocketHandler.getInstance().send("onRoundEnd", payload);
-          setRemainTurnTimeNumber(0);
-          setRemainRoundTimeNumber(0);
-          setTimeGauge(0);
-          return;
         }
+        setRemainTurnTimeNumber(0);
+        setRemainRoundTimeNumber(0);
+        setTimeGauge(0);
+        return;
       } else {
         setRemainTurnTimeNumber((prev) => prev - 100 > 0 ? prev - 100 : 0);
         setRemainRoundTimeNumber((prev) => prev - 100 > 0 ? prev - 100 : 0);
